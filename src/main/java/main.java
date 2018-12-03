3130038a5a24806e53b08b051cd9a51b4e2b5c1b
António Alves <troncalias@gmail.com>
2018-12-03 14:03:52 +0000

    Mensagem

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public void test1(int score){
+        if (score < 45) {
+            System.out.println("faile");
+        }else{
+            System.out.println("pass");
+            if (score >80){
+                System.out.println("with distinction");
+            }
+        }
+    }
+}
---
+++ src/test/java/test1.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+public class test1 {
+    @Test
+    public void test_1(){
+        main m = new main();
+
+    }
+
+    @Test
+    public void test_2(){
+
+    }
+
+    @Test
+    public void test_3(){
+
+    }
+}
